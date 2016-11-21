aa42fe1dbc38347de5a6b71a6c5c889ab998b4eb
ragnar <ragnarhardarson90@gmail.com>
2016-11-21 19:13:02 +0100

    removing unused vars

--- tests/components/Header/Header.spec.js
+++ tests/components/Header/Header.spec.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Header } from 'components/Header/Header'
-import { IndexLink, Link } from 'react-router'
 import { shallow } from 'enzyme'
 
 describe('(Component) Header', () => {
