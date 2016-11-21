786493054832f0e24183e8e1f9a57bc5fb33795e
ragnar <ragnarhardarson90@gmail.com>
2016-11-21 22:13:52 +0100

    Adding app.js for Azure

--- app.js
+++ app.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 8080
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/dist'))
 
 // handle every other route with index.html, which will contain
 // a script tag to your application's JavaScript file(s).
 app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+  response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
 app.listen(port)
